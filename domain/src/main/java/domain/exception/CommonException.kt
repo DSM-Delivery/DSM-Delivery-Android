9fa3a358f2788c2bbc70323d63b8a96734cc5d07
Android No.1 <yeon8747@naver.com>
2023-03-08 23:28:25 +0900

    ✨ :: implement domain Exception

---
+++ domain/src/main/java/domain/exception/CommonException.kt
@@ -0,0 +1,17 @@
+package domain.exception
+
+class BadRequestException: RuntimeException()
+
+class ConflictException: RuntimeException()
+
+class ForbiddenException: RuntimeException()
+
+class NoInternetExecption: RuntimeException()
+
+class NotFoundException: RuntimeException()
+
+class ServerException: RuntimeException()
+
+class UnknownException: RuntimeException()
+
+class ToManyRequestException: RuntimeException()
