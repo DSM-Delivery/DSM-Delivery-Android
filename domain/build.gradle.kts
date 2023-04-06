7d660fb6d06075448c8822f29d6e58889e09795c
Kim Yeon woo <yeon8747@naver.com>
2023-04-06 09:40:39 +0900

    🗂 :: domain layer file setting

---
+++ domain/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id(BuildPlugins.ANDROID_LIBRARY_PLUGIN)
+    id(BuildPlugins.DAGGER_HILT_PLUGIN)
+    id(BuildPlugins.KOTLIN_ANDROID_PLUGIN)
+    id(BuildPlugins.KOTLIN_KAPT)
+}
+
+android {
+    namespace = "dsm.delivery.domain"
+    compileSdk = ProjectProperties.COMPILE_SDK_VERSION
+
+    defaultConfig {
+        minSdk = ProjectProperties.MIN_SDK_VERSION
+        targetSdk = ProjectProperties.TARGET_SDK_VERSION
+
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    kotlinOptions {
+        jvmTarget = ProjectProperties.JAVA_VERSION.toString()
+    }
+}
+
+dependencies {
+
+
+    implementation(Dependency.UnitTest.JUNIT)
+
+    implementation(Dependency.Ui.LOCALDATETIME)
+
+    implementation(Dependency.Hilt.HILT_ANDROID)
+    implementation(Dependency.Hilt.INJECT)
+    kapt(Dependency.Hilt.HILT_ANDROID_COMPILER)
+
+    implementation(Dependency.Kotlin.COROUTINES_CORE)
+    implementation(Dependency.Kotlin.COROUTINES_ANDROID)
+}
