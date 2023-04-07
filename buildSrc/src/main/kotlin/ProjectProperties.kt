1a429b0d0c521264de8a880da170c7e072e32df8
Kim Yeon woo <yeon8747@naver.com>
2023-04-07 19:14:57 +0900

    🐞 ::	lintDebug error 수정

--- buildSrc/src/main/kotlin/ProjectProperties.kt
+++ buildSrc/src/main/kotlin/ProjectProperties.kt
@@ -16,6 +16,6 @@ object ProjectProperties {
     const val BUILD_GRADLE = "4.2.1"
     const val COMPILE_SDK_VERSION = 33
     const val BUILD_TOOLS_VERSION = "33.0.0"
-    const val MIN_SDK_VERSION = 30
+    const val MIN_SDK_VERSION = 26
     const val TARGET_SDK_VERSION = 33
 }
